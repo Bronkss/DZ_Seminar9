b3cb104a75f422d2cfbefa3ed78098bbc78bad7d
Alex <alexandr.yakimov.heaven@gmail.com>
2022-12-16 12:41:09 +0500

    Task is done

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+System.Console.WriteLine(NaturalNumber(5,1));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m) return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine(SumRex(4, 8));
+int SumRex(int m, int n)
+{
+    if (m > n) return 0;
+    else return m + SumRex(m + 1, n);
+}
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine(Akkerman(3,2));
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return Akkerman(m - 1,(Akkerman(m, n - 1)));
+    return Akkerman(m,n);
+}
